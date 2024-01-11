32e4c44da154888c90a3c9433be031599c2f5938
Antonin Lmp <73228586+AntoninLmp@users.noreply.github.com>
2024-01-12 00:17:58 +0100

    feat(Student): update student page

--- src/app/[lang]/student/page.tsx
+++ src/app/[lang]/student/page.tsx
@@ -1,13 +1,40 @@
 import type React from "react";
 import { getDictionnary, type Locale } from "@lib/getDictionnary";
+import { CustomTable } from "@src/app/modules/CustomTable";
 
 export default async function Page({ params }: { params: { lang: string } }) {
   const dictionary = await getDictionnary(params.lang as Locale);
   console.log(dictionary);
 
+  /*CustomTable data + column name*/
+  const column = ["Date", "Company", "Contact", "Internship", "Document"];
+  const data = [
+    {
+      Date: "2022-01-01",
+      Company: "ABC Inc",
+      Contact: "John Doe",
+      Internship: "Software Developer",
+      Document: "Resume",
+    },
+    {
+      Date: "2022-02-01",
+      Company: "XYZ Ltd",
+      Contact: "Jane Smith",
+      Internship: "Marketing Intern",
+      Document: "Cover Letter",
+    },
+  ];
+
   return (
-    <main>
-      <h1>Hello</h1>
+    <main className={"px-20 py-16 text-blue-900"}>
+      <h1 className={"py-7 text-4xl font-semibold italic "}>Student</h1>
+      <section>
+        <h2 className={"py-4 text-2xl underline"}>My Internships</h2>
+        <CustomTable columns={column} data={data} />
+      </section>
+      <section>
+        <h2 className={"py-4 text-2xl underline"}>Add internship</h2>
+      </section>
     </main>
   );
 }
