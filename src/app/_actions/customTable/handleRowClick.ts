141351e81d3eda5c1525f4e069872fc07861a642
Aurélien <aureliendp@free.fr>
2024-01-22 20:04:55 +0100

    Feat(AdminPage): Added status update for selected internship

--- src/app/_actions/customTable/handleRowClick.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-export const handleClick = (rowIndex: number, pageRedirectOnClick: string) => {
-  if (pageRedirectOnClick == "adm") {
-    return `/adm/${rowIndex}`;
-  }
-  return "#";
-};
