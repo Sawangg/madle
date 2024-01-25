ddfaec6db4124dd7bb6cd05d69044423adc8e4fc
Kylian Sauvee <kylian@sauvee.com>
2024-01-25 16:12:48 +0100

    feat: added addInternship action

---
+++ src/app/_actions/addInternship.ts
@@ -0,0 +1,59 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { z } from "zod";
+import { companies, internships } from "@db/schema";
+import { db } from "@src/db";
+
+const AddInternshipSchema = z.object({
+  companyName: z.string(),
+  companyAddress: z.string(),
+  companyCity: z.string(),
+  companyPostalCode: z.string(),
+  contactName: z.string(),
+  contactEmail: z.string(),
+  dateStart: z.string(),
+  dateEnd: z.string(),
+  title: z.string(),
+  studentId: z.string(),
+});
+
+export const addInternship = async (data: {
+  companyName: string;
+  companyAddress: string;
+  companyCity: string;
+  companyPostalCode: string;
+  contactName: string;
+  contactEmail: string;
+  dateStart: string;
+  dateEnd: string;
+  title: string;
+  studentId: string;
+}) => {
+  const result = AddInternshipSchema.safeParse(data);
+  if (!result.success) return { message: result.error };
+
+  const insertCompanyResult = await db
+    .insert(companies)
+    .values({
+      name: result.data.companyName,
+      address: result.data.companyAddress,
+      city: result.data.companyCity,
+      postalCode: result.data.companyPostalCode,
+    })
+    .returning({ insertedId: companies.id });
+  const insertedCompanyId = insertCompanyResult[0].insertedId;
+
+  await db.insert(internships).values({
+    companyId: insertedCompanyId,
+    contactName: result.data.contactName,
+    contactEmail: result.data.contactEmail,
+    dateStart: new Date(result.data.dateStart),
+    dateEnd: new Date(result.data.dateEnd),
+    status: "pending",
+    title: result.data.title,
+    studentId: result.data.studentId,
+  });
+
+  return redirect("/student");
+};
